6974216989240f3e10c171e6127fb38c2502b735
Theo Bearman <theo@theobearman.com>
2024-01-17 13:05:18 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,8 @@ export default function Page() {
                 <CardHeader>
                   <div className="flex items-center justify-between gap-x-2 text-base">
                     <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
-                    </h3>
+                     <a> {work.company} </a>
+                  </h3>
                     <div className="text-sm tabular-nums text-gray-500">
                       {work.start} - {work.end}
                     </div>
