8fe77ab1c263a920ba34b35844c2b5366147e219
Theo Bearman <theo@theobearman.com>
2026-01-07 12:32:30 +0000

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
